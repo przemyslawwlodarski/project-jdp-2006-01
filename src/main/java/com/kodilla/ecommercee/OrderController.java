1b1bf572691111ade4dc93e33097d4a0535a0491
kamilkowalczyk27 <kamilkowalczyk27@o2.pl>
2020-06-26 20:33:41 +0200

    Adding OrderController

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -1,4 +1,36 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domains.OrderDto;
+import com.kodilla.ecommercee.domains.UserDto;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping("/v1/order")
 public class OrderController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "getOrders")
+    public List<OrderDto> getOrders() {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getOrder")
+    public OrderDto getOrder(@RequestParam Long orderId)  {
+        return new OrderDto(1L, new ArrayList<>(),  new UserDto(1L, "Kowalski"));
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createOrder", consumes = APPLICATION_JSON_VALUE)
+    public void createOrder(@RequestBody OrderDto orderDto) {
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateOrder")
+    public OrderDto updateOrder(@RequestBody OrderDto orderDto) {
+        return new OrderDto(1L, new ArrayList<>(), new UserDto(1L, "Nowak"));
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteOrder")
+    public void deleteOrder(@RequestParam Long orderId) {
+    }
 }
