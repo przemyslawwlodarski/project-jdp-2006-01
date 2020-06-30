89504e0fecc9c443e8014fd4518993b31efd42a1
kamilkowalczyk27 <kamilkowalczyk27@o2.pl>
2020-06-30 22:50:40 +0200

    Adding Entity Order Repository

---
+++ src/main/java/com/kodilla/ecommercee/repositories/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.kodilla.ecommercee.repositories;
+
+import com.kodilla.ecommercee.domains.Order;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
+public interface OrderRepository extends CrudRepository<Order, Long> {
+}
