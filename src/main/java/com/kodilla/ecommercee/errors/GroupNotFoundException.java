ae72739552c0f6d33cbd744c6e6754a671e382e4
przemyslawwlodarski <60506313+przemyslawwlodarski@users.noreply.github.com>
2020-07-14 20:09:26 +0200

    Update GroupNotFoundException.java

--- src/main/java/com/kodilla/ecommercee/errors/GroupNotFoundException.java
+++ src/main/java/com/kodilla/ecommercee/errors/GroupNotFoundException.java
@@ -1,5 +1,8 @@
 package com.kodilla.ecommercee.errors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "group not found")
 public class GroupNotFoundException extends Exception {
     public GroupNotFoundException(String message) {
