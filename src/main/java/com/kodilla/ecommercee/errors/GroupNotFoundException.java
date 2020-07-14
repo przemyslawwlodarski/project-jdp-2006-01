e1049becc602ace29dd6b2a1fe7f18c052be8ef6
przemyslawwlodarski <przemyslaw.wlodarski@gmail.com>
2020-07-14 19:38:56 +0200

    GroupController modified GroupDbService added GroupMapper added GroupNotFoundException added GroupRepository added

--- src/main/java/com/kodilla/ecommercee/errors/GroupNotFoundException.java
+++ src/main/java/com/kodilla/ecommercee/errors/GroupNotFoundException.java
@@ -1,4 +1,11 @@
 package com.kodilla.ecommercee.errors;
 
 public class GroupNotFoundException extends Exception {
+    public GroupNotFoundException(String message) {
+        super(message);
+    }
+
+    public GroupNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
