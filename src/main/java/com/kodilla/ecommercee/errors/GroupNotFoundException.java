bed5cb5f9a8b96e3acebfce059f791ab3b5fa81f
przemyslawwlodarski <60506313+przemyslawwlodarski@users.noreply.github.com>
2020-07-14 19:51:59 +0200

    Update GroupNotFoundException.java

--- src/main/java/com/kodilla/ecommercee/errors/GroupNotFoundException.java
+++ src/main/java/com/kodilla/ecommercee/errors/GroupNotFoundException.java
@@ -1,5 +1,6 @@
 package com.kodilla.ecommercee.errors;
 
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "group not found")
 public class GroupNotFoundException extends Exception {
     public GroupNotFoundException(String message) {
         super(message);
